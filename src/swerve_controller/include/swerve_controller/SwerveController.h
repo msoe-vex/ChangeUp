fadd6fdc59eb47b78388c5ad52c6abff372203b3
Andrew Kempen <akemperfi@gmail.com>
2021-01-21 01:00:11 +0000

    Reset swerve_controller files to main

--- src/swerve_controller/include/swerve_controller/SwerveController.h
+++
@@ -1,9 +0,0 @@
-#include <Eigen/Dense>
-#include "swerve_controller/SwerveModule.h"
-#include <vector>
-
-class SwerveController {
-public:
-    SwerveController(std::vector<SwerveModule> swerveModules);
-    void InverseKinematics(Eigen::Vector2d targetVelocity, Eigen::Rotation2Dd targetRotationVelocity);
-};
