4d9ece6fbf7b0892dfa5e2e1b684c22df7dc13ae
Dylan Powers <dylanpowers125@gmail.com>
2021-01-21 20:52:52 -0600

    Fixed ADIDigitalNode

--- src/v5_hal/firmware/include/DataNodes/ADIDigitalOutNode.h
+++ src/v5_hal/firmware/include/DataNodes/ADIDigitalOutNode.h
@@ -2,13 +2,15 @@
 
 #include "api.h"
 #include "NodeManager.h"
+#include "ros_lib/ros.h"
+#include "ros_lib/std_msgs/Bool.h"
 
 class ADIDigitalOutNode : public Node {
 private:
     pros::ADIDigitalOut m_digital_out;
     std::string m_handle_name;
-    ros::Subscriber<std_msgs::Bool, ADIDigitalOutNode>* m_digital_out_sub;
     std::string m_sub_digital_out_name;
+    ros::Subscriber<std_msgs::Bool, ADIDigitalOutNode>* m_digital_out_sub;
 
     void m_setValue ();
 
