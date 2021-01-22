53a31bc9127c9abebbb25bf6c8d45dff76acf22f
Dylan Powers <dylanpowers125@gmail.com>
2021-01-21 20:34:31 -0600

    Finished ADIDigitalOutNode

--- src/v5_hal/firmware/include/DataNodes/ADIDigitalOutNode.h
+++ src/v5_hal/firmware/include/DataNodes/ADIDigitalOutNode.h
@@ -7,12 +7,14 @@ class ADIDigitalOutNode : public Node {
 private:
     pros::ADIDigitalOut m_digital_out;
     std::string m_handle_name;
-    ros::Subscriber<std_msgs::Bool, ADIDigitalOutNode> m_digital_out_sub;
-    std::string m_
+    ros::Subscriber<std_msgs::Bool, ADIDigitalOutNode>* m_digital_out_sub;
+    std::string m_sub_digital_out_name;
+
+    void m_setValue ();
 
 public:
     ADIDigitalOutNode (NodeManager* node_manager, std::string handle_name,
-    std::uint8_t port, bool initial_state = false);
+    int port, bool initial_state = false);
 
     void initialize ();
 
--- src/v5_hal/firmware/src/DataNodes/ADIDigitalOutNode.cpp
+++ src/v5_hal/firmware/src/DataNodes/ADIDigitalOutNode.cpp
@@ -1,15 +1,23 @@
 #include "DataNodes/ADIDigitalOutNode.h"
 
 ADIDigitalOutNode::ADIDigitalOutNode (NodeManager* node_manager, std::string handle_name,
-    std::uint8_t port, bool initial_state = false) : Node (nodeManager, 20), 
-    ADIDigitalOut (port, intial_state) {
-    m_handle_name = handle_name;
+    int port, bool initial_state = false) : Node (nodeManager, 20), 
+    m_digital_out (port, intial_state) {
+    m_handle_name = handle_name.insert(0, "output/");
+    m_sub_digital_out_name = m_handle_name + "/digitalOut";
+    m_digital_out_sub = new ros::Subscriber<std_msgs::Bool, ADIDigitalOutNode>
+        m_sub_digital_out_name.c_str(), &ADIDigitlOutNode::m_setValue, this);
     }
 
-ADIDigitalOutNode::initialize () {
+void ADIDigitlOutNode::m_setValue (const std_msgs::Bool& msg) {
+    m_digital_out.set_value((int)msg.data);
+}
 
+void ADIDigitalOutNode::initialize () {
+    Node::m_handle->initNode();
+    Node::m_handle->subscribe(*m_digital_out_sub);
 }
 
 ADIDigitalOutNode::~ADIDigitalOutNode () {
-    
+    delete m_digital_out_sub;
 }
