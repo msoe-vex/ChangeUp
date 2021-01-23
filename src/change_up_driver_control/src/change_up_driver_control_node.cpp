39e549fec77d7a81754c483913b081fb54b8ce0b
NathanDuPont <nathan.dupont01@gmail.com>
2021-01-22 18:22:39 -0600

    Added motor publishers in driver control node

--- src/change_up_driver_control/src/change_up_driver_control_node.cpp
+++ src/change_up_driver_control/src/change_up_driver_control_node.cpp
@@ -7,29 +7,30 @@
 
 std_msgs::Int8 msg1;
 
-void primaryJoystickCallback(const v5_hal::V5Controller::ConstPtr& joystick ) {
-    ROS_INFO("%i", msg->btn_right);
+void primaryJoystickCallback(const v5_hal::V5Controller::ConstPtr& joystick) {
     msg1.data = msg->analog_left_y; 
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "controllers/changeUpDriverControl");
 
-  ros::NodeHandle nh;
+  ros::NodeHandle handle;
 
-  ros::Subscriber driverJoystick = nh.subscribe("/controller/primary", 100, primaryJoystickCallback);
+  ros::Subscriber primary_joystick_sub = handle.subscribe("/controller/primary", 100, primaryJoystickCallback);
 
-   ros::Publisher chatter_pub = nh.advertise<std_msgs::Int8>("/motor/leftSwerve1/moveMotorVoltage", 100);
+  ros::Publisher left_intake_pub = handle.advertise<std_msgs::Int8>("/motor/leftIntake/moveMotorVoltage", 100);
+  ros::Publisher right_intake_pub = handle.advertise<std_msgs::Int8>("/motor/rightIntake/moveMotorVoltage", 100);
+  
+  ros::Publisher bottom_roller_pub = handle.advertise<std_msgs::Int8>("/motor/bottomRollers/moveMotorVoltage", 100);
+  ros::Publisher top_rollers_pub = handle.advertise<std_msgs::Int8>("/motor/topRollers/moveMotorVoltage", 100);
 
 
-  ros::Rate loop_rate(100);  
+  ros::Rate loop_rate(50);  
 
   int count = 0;
   while (ros::ok())
   {
-
-
     chatter_pub.publish(msg1);
 
     ros::spinOnce();
