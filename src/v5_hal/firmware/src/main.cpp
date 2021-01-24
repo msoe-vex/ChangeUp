ce3ed4bfa0cfc0fac4b84d3f461b1bf5af7e7dff
Andrew Kempen <akemperfi@gmail.com>
2021-01-24 03:21:56 -0600

    Test code for using V5 port for communication

--- src/v5_hal/firmware/src/main.cpp
+++ src/v5_hal/firmware/src/main.cpp
@@ -1,28 +1,28 @@
 #include "main.h"
-#include "NodeManager.h"
 
-NodeManager* node_manager = new NodeManager(pros::millis);
+
+// NodeManager* node_manager = new NodeManager(pros::millis);
 
 // Declare all nodes here
-ControllerNode* primary_controller;
-MotorNode* left_module_1;
-MotorNode* left_module_2;
-ADIAnalogInNode* left_module_pot;
-MotorNode* right_module_1;
-MotorNode* right_module_2;
-ADIAnalogInNode* right_module_pot;
-MotorNode* rear_module_1;
-MotorNode* rear_module_2;
-ADIAnalogInNode* rear_module_pot;
-MotorNode* left_intake;
-MotorNode* right_intake;
-MotorNode* bottom_rollers;
-MotorNode* top_rollers;
-ADIEncoderNode* x_odometry_encoder;
-ADIEncoderNode* y_odometry_encoder;
-BatteryNode* battery;
-CompetitionStatusNode* competition_status;
-ProsTimeNode* pros_time;
+// ControllerNode* primary_controller;
+// MotorNode* left_module_1;
+// MotorNode* left_module_2;
+// ADIAnalogInNode* left_module_pot;
+// MotorNode* right_module_1;
+// MotorNode* right_module_2;
+// ADIAnalogInNode* right_module_pot;
+// MotorNode* rear_module_1;
+// MotorNode* rear_module_2;
+// ADIAnalogInNode* rear_module_pot;
+// MotorNode* left_intake;
+// MotorNode* right_intake;
+// MotorNode* bottom_rollers;
+// MotorNode* top_rollers;
+// ADIEncoderNode* x_odometry_encoder;
+// ADIEncoderNode* y_odometry_encoder;
+// BatteryNode* battery;
+// CompetitionStatusNode* competition_status;
+// ProsTimeNode* pros_time;
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -32,34 +32,34 @@ ProsTimeNode* pros_time;
  */
 void initialize() {
 	// Define all nodes used by the robot here
-	primary_controller = new ControllerNode(node_manager, "primary");
+	// primary_controller = new ControllerNode(node_manager, "primary");
 
-	left_module_1 = new MotorNode(node_manager, 1, "leftModule1", false);
-	left_module_2 = new MotorNode(node_manager, 2, "leftModule2", false);
-	left_module_pot = new ADIAnalogInNode(node_manager, 8, "leftModulePot");
+	// left_module_1 = new MotorNode(node_manager, 1, "leftModule1", false);
+	// left_module_2 = new MotorNode(node_manager, 2, "leftModule2", false);
+	// left_module_pot = new ADIAnalogInNode(node_manager, 8, "leftModulePot");
 
-	right_module_1 = new MotorNode(node_manager, 3, "rightModule1", false);
-	right_module_2 = new MotorNode(node_manager, 4, "rightModule2", false);
-	right_module_pot = new ADIAnalogInNode(node_manager, 7, "rightModulePot");
+	// right_module_1 = new MotorNode(node_manager, 3, "rightModule1", false);
+	// right_module_2 = new MotorNode(node_manager, 4, "rightModule2", false);
+	// right_module_pot = new ADIAnalogInNode(node_manager, 7, "rightModulePot");
 
-	rear_module_1 = new MotorNode(node_manager, 5, "rearModule1", false);
-	rear_module_2 = new MotorNode(node_manager, 6, "rearModule2", false);
-	rear_module_pot = new ADIAnalogInNode(node_manager, 6, "rearModulePot");
+	// rear_module_1 = new MotorNode(node_manager, 5, "rearModule1", false);
+	// rear_module_2 = new MotorNode(node_manager, 6, "rearModule2", false);
+	// rear_module_pot = new ADIAnalogInNode(node_manager, 6, "rearModulePot");
 	
-	left_intake = new MotorNode(node_manager, 7, "leftIntake", false);
-	right_intake = new MotorNode(node_manager, 8, "rightIntake", false);
-	bottom_rollers = new MotorNode(node_manager, 9, "bottomRollers", false);
-	top_rollers = new MotorNode(node_manager, 10, "topRollers", false);
+	// left_intake = new MotorNode(node_manager, 7, "leftIntake", false);
+	// right_intake = new MotorNode(node_manager, 8, "rightIntake", false);
+	// bottom_rollers = new MotorNode(node_manager, 9, "bottomRollers", false);
+	// top_rollers = new MotorNode(node_manager, 10, "topRollers", false);
 	
-	x_odometry_encoder = new ADIEncoderNode(node_manager, 1, 2, "xOdometryEncoder", false);
-	y_odometry_encoder = new ADIEncoderNode(node_manager, 3, 4, "yOdometryEncoder", false);
+	// x_odometry_encoder = new ADIEncoderNode(node_manager, 1, 2, "xOdometryEncoder", false);
+	// y_odometry_encoder = new ADIEncoderNode(node_manager, 3, 4, "yOdometryEncoder", false);
 	
-	battery = new BatteryNode(node_manager, "v5battery");
-	competition_status = new CompetitionStatusNode(node_manager, "competitionStatus");
-	pros_time = new ProsTimeNode(node_manager, "prosTime");
+	// battery = new BatteryNode(node_manager, "v5battery");
+	// competition_status = new CompetitionStatusNode(node_manager, "competitionStatus");
+	// pros_time = new ProsTimeNode(node_manager, "prosTime");
 
-	// Call the node manager to initialize all of the nodes above
-	node_manager->initialize();
+	// // Call the node manager to initialize all of the nodes above
+	// node_manager->initialize();
 }
 
 /**
@@ -111,7 +111,19 @@ void autonomous() {}
  * and adding a wait to this thread will disrupt the performance of all nodes.
  */
 void opcontrol() {
-	while (true) {
-		node_manager->execute();
+	pros::c::serial_enable(20);
+	pros::c::serial_flush(20);
+	pros::c::serial_set_baudrate(20, 115200);
+	int i = 0;
+	while(true) {
+		pros::c::serial_write_byte(20, i);
+		pros::delay(1000);
+		i++;
+		if(i>15) {
+			i = 0;
+		}
 	}
+	// while (true) {
+	// 	node_manager->execute();
+	// }
 }
