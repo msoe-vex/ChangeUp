06a7a224c3c204c2d1bc3b0637366a7672687d76
Quinten Robley <qprtechman@gmial.com>
2021-01-28 19:53:36 -0600

    Added ADIAnalogOutNode

---
+++ src/v5_hal/firmware/include/DataNodes/ADIAnalogOutNode.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "api.h"
+#include "NodeManager.h"
+#include "ros_lib/ros.h"
+#include "ros_lib/std_msgs/Int16.h"
+
+class ADIAnalogOutNode : public Node {
+
+private:
+    pros::ADIAnalogOut m_analog_out;
+    std::string m_handle_name;
+    std::string m_sub_analog_out_name;
+    ros::Subscriber<std_msgs::Int16, ADIAnalogOutNode>* m_analog_out_sub;
+
+    void m_setValue(const std_msgs::Int16& msg);
+
+public:
+    ADIAnalogOutNode(NodeManager* node_manager, std::string handle_name,
+    int port);
+
+    void initialize();
+
+    void periodic();
+
+    ~ADIAnalogOutNode();
+};
