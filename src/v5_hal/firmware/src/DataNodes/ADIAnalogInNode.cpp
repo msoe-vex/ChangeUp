43b8aeb1e5717621cd128e3b881111cb4ecc2191
Quinten Robley <qprtechman@gmial.com>
2021-01-30 00:30:28 -0600

    Major code changes! Add motor functionality and fixed a lot of bugs

--- src/v5_hal/firmware/src/DataNodes/ADIAnalogInNode.cpp
+++ src/v5_hal/firmware/src/DataNodes/ADIAnalogInNode.cpp
@@ -24,10 +24,6 @@ void ADIAnalogInNode::periodic() {
     Node::m_handle->spinOnce();
 }
 
-int16_t ADIAnalogInNode::getValue(){
-    return m_analog_in.get_value();
-}
-
 void ADIAnalogInNode::m_populateMessage() {
     if (m_is_reversed) {
         m_analog_in_msg.data = (4096 - m_analog_in.get_value());
--- src/v5_hal/firmware/src/DriverControlNode.cpp
+++ src/v5_hal/firmware/src/DriverControlNode.cpp
@@ -1,13 +1,16 @@
-#include "eigen/Eigen/Dense"
-#include "math.h"
-#include "SwerveController.h"
-#include "DataNodes/DriverControlNode.h"
+#include "DriverControlNode.h"
 
 // By default, this constructor calls the constructor for the Node object in
 // NodeManager.h
-DriverControlNode::DriverControlNode(NodeManager* node_manager, 
-    std::string handle_name) : Node(node_manager, 20) {
-    m_handle_name = handle_name.insert(0, "driveControl/");
+DriverControlNode::DriverControlNode(NodeManager* node_manager, pros::Motor left_swerve_1, pros::Motor left_swerve_2, 
+        pros::ADIAnalogIn left_swerve_pot, pros::Motor right_swerve_1, pros::Motor right_swerve_2, 
+        pros::ADIAnalogIn right_swerve_pot, pros::Motor rear_swerve_1, pros::Motor rear_swerve_2, 
+        pros::ADIAnalogIn rear_swerve_pot) : Node(node_manager, 20), 
+    swerveController(left_module_location, right_module_location, rear_module_location, rotation_angle_threshold,
+    max_velocity, max_rotation_velocity, kP, kI, kD), left_swerve_1(left_swerve_1), left_swerve_2(left_swerve_2),
+    left_swerve_pot(left_swerve_pot), right_swerve_1(right_swerve_1), right_swerve_2(right_swerve_2),
+    right_swerve_pot(right_swerve_pot), rear_swerve_1(rear_swerve_1), rear_swerve_2(rear_swerve_2),
+    rear_swerve_pot(rear_swerve_pot) {
 
     left_module_location(0) = left_module_location_x;
     left_module_location(1) = left_module_location_y;
@@ -16,25 +19,24 @@ DriverControlNode::DriverControlNode(NodeManager* node_manager,
     rear_module_location(0) = rear_module_location_x;
     rear_module_location(1) = rear_module_location_y;
 
-    SwerveController(left_module_location, right_module_location, rear_module_location, rotation_angle_threshold,
-        max_velocity, max_rotation_velocity, kP, kI, kD);
+    left_swerve_1.move(-127);
 
 }
 
 void DriverControlNode::initialize() {
-    // Initialize the handler, and set up data to publish
     Node::m_handle->initNode();
 }
 
 void DriverControlNode::periodic() {
-    // Publish data when called, and spin the handler to send data to the
-    // coprocessor on the published topic
-    Node::m_handle->spinOnce();
 
-    assignActualAngle(0.0, 0.0, 0.0);
-    calculateLeftModule(target_velocity, rotation_velocity);
-    calculateRightModule(target_velocity, rotation_velocity);
-    calculateRearModule(target_velocity, rotation_velocity);
+    swerveController.assignActualAngle(left_swerve_pot.get_value(), right_swerve_pot.get_value(), rear_swerve_pot.get_value());
+    left_swerve_1.move(swerveController.calculateLeftModule(target_velocity, rotation_velocity).left_motor_power);
+    left_swerve_2.move(swerveController.calculateLeftModule(target_velocity, rotation_velocity).right_motor_power);
+    right_swerve_1.move(swerveController.calculateRightModule(target_velocity, rotation_velocity).left_motor_power);
+    right_swerve_2.move(swerveController.calculateRightModule(target_velocity, rotation_velocity).right_motor_power);
+    rear_swerve_1.move(swerveController.calculateRearModule(target_velocity, rotation_velocity).left_motor_power);
+    rear_swerve_2.move(swerveController.calculateRearModule(target_velocity, rotation_velocity).right_motor_power);
+
 }
 
 DriverControlNode::~DriverControlNode() { 
