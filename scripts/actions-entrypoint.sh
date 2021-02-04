8ba0d9a2dea06b64a5786f4addfc4c6cb9fed1db
NathanDuPont <nathan.dupont01@gmail.com>
2021-02-04 01:17:29 -0600

    Added rosdep command to pull in tf package

--- scripts/actions-entrypoint.sh
+++ scripts/actions-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 apt-get update
-apt-get install -y python-pip
-apt-get install -y ros-noetic-catkin python-catkin-tools
-apt-get install -y ros-noetic-roslint
+apt-get install -y python3 python3-pip
+apt-get install -y python3-rosdep
