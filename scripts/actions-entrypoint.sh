11a30f5777b2623df219b38fd4d0131cc3d17391
NathanDuPont <nathan.dupont01@gmail.com>
2021-02-05 11:49:56 -0600

    Re-adding variable to GITHUB_PATH

--- scripts/actions-entrypoint.sh
+++ scripts/actions-entrypoint.sh
@@ -4,9 +4,10 @@ sudo apt-get install -y python3-rosdep
 sudo apt-get install -y ros-noetic-tf
 sudo apt-get install -y wget
 
-cd ~
+cd $HOME
 wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2 --no-check-certificate
 tar -xjvf gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2
+echo "$HOME/gcc-arm-none-eabi-9-2020-q2-update/bin/" >> $GITHUB_PATH
 
 # Install PROS
 sudo python -m pip install https://github.com/purduesigbots/pros-cli/releases/download/3.1.4/pros_cli_v5-3.1.4-py3-none-any.whl
