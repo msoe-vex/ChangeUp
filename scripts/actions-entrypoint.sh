ec462bca9fa8303a353967097f9755141a5e97b5
NathanDuPont <nathan.dupont01@gmail.com>
2021-02-05 03:13:10 -0600

    Adding install for wget

--- scripts/actions-entrypoint.sh
+++ scripts/actions-entrypoint.sh
@@ -2,6 +2,7 @@
 sudo apt-get install -y python3 python3-pip python-is-python3
 sudo apt-get install -y python3-rosdep
 sudo apt-get install -y ros-noetic-tf
+sudo apt-get install -y wget
 
 cd ~
 wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2 --no-check-certificate
