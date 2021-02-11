507ffe36ee8685c744b9a1ffac2b4a94dac47970
TrentonBowser <trenton.bowser25@gmail.com>
2021-02-10 20:58:47 -0600

    Added drive action

--- src/v5_hal/firmware/include/Actions/DriveAction.h
+++ src/v5_hal/firmware/include/Actions/DriveAction.h
@@ -2,15 +2,19 @@
 
 #include "api.h"
 #include "Auton.h"
-#include "Timer.h"
 #include "DataNodes/TankDriveNode.h"
 
-class DriveAction : public DriveAction {
+class DriveAction : public AutonAction {
 private:
     TankDriveNode* m_tank_drive_node;
-    Timer m_timer;
+    double m_distance_left;
+    double m_distance_right;
+    int m_max_velocity;
+    int m_actual_left_distance;
+    int m_actual_right_distance;
+
 public:
-    DriveAction(TankDriveNode* tank_drive_node, double distance);
+    DriveAction(TankDriveNode* tank_drive_node, double distance_left, double distance_right, int max_velocity);
 
     void actionInit();
     actionStatus action();
--- src/v5_hal/firmware/src/Actions/DriveAction.cpp
+++ src/v5_hal/firmware/src/Actions/DriveAction.cpp
@@ -1,18 +1,21 @@
 #include "Actions/DriveAction.h"
+#include <math.h>
 
-DriveAction::DriveAction(TankDriveNode* tank_drive_node) :
-        m_tank_drive_node(tank_drive_node) {
+DriveAction::DriveAction(TankDriveNode* tank_drive_node, double distance_left, double distance_right, int max_velocity) :
+        m_tank_drive_node(tank_drive_node), m_distance_left(distance_left), m_distance_right(distance_right), m_max_velocity(max_velocity) {
 
 }
 
 void DriveAction::actionInit() {
-    m_timer.Start();
+    m_tank_drive_node->setDriveDistancePID(m_distance_left, m_distance_right, m_max_velocity);
 }
 
 AutonAction::actionStatus DriveAction::action() {
-    
+    m_actual_left_distance = m_tank_drive_node->getLeftDistancePID();
+    m_actual_right_distance = m_tank_drive_node->getRightDistancePID();
 
-    if(m_timer.Get() > 1) {
+    if(fabs(m_actual_left_distance - m_distance_left) < 128 &&
+        fabs(m_actual_right_distance - m_distance_right) < 128) {
         return END;
     } else {
         return CONTINUE;
@@ -20,5 +23,5 @@ AutonAction::actionStatus DriveAction::action() {
 }
 
 void DriveAction::actionEnd() {
-    m_timer.Stop();
+    
 }
