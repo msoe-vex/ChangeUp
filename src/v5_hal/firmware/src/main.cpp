f66d6dc7066e2bd2c52bc1f491cbb121f0e496fa
NathanDuPont <nathan.dupont01@gmail.com>
2021-02-10 19:55:03 -0600

    Added intake action

--- src/v5_hal/firmware/src/main.cpp
+++ src/v5_hal/firmware/src/main.cpp
@@ -111,7 +111,10 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+
+	
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
