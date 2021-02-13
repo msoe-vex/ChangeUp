cc7b08f71e39cca9a13ad9ea8d38e17ff84c9122
NathanDuPont <nathan.dupont01@gmail.com>
2021-02-12 20:52:23 -0600

    Updated inertial sensor to be non-blocking and support ROS sensors, and update references to RPY instead of roll

--- src/v5_hal/firmware/include/DataNodes/InertialSensorNode.h
+++ src/v5_hal/firmware/include/DataNodes/InertialSensorNode.h
@@ -3,20 +3,29 @@
 #include "NodeManager.h"
 #include "api.h"
 #include "ros_lib/ros.h"
+#include "ros_lib/v5_hal/RollPitchYaw.h"
 
 class InertialSensorNode : public Node {
-private:
-    pros::Imu m_inertial_sensor;
-    std::string m_handle_name;
-    double turning_threshold = 0.1;
-
 public:
-    InertialSensorNode(NodeManager* node_manager, int sensor_port, 
-        std::string handle_name);
+    enum SensorConfig {
+        V5, ROS
+    };
+
+    struct RPY {
+        double roll;
+        double pitch;
+        double yaw;
+    };
+
+    InertialSensorNode(NodeManager* node_manager, std::string handle_name, 
+        int sensor_port);
+
+    InertialSensorNode(NodeManager* node_manager, std::string handle_name, 
+        std::string subscribe_handle);
     
     void initialize();
     
-    double getYaw();
+    RPY getRPY();
 
     bool isAtAngle(double angle);
     
@@ -25,4 +34,16 @@ public:
     void autonPeriodic();
     
     ~InertialSensorNode();
+
+private:
+    pros::Imu* m_inertial_sensor = nullptr;
+    std::string m_handle_name;
+    std::string m_sub_inertial_sensor_name;
+    double turning_threshold = 0.1;
+    SensorConfig m_config;
+    RPY m_rpy = {0, 0, 0};
+
+    ros::Subscriber<v5_hal::RollPitchYaw, InertialSensorNode>* m_inertial_sensor_sub = nullptr;
+
+    void m_handleSensorMsg(const v5_hal::RollPitchYaw& msg);
 };
--- src/v5_hal/firmware/include/DataNodes/OdometryNode.h
+++ src/v5_hal/firmware/include/DataNodes/OdometryNode.h
@@ -3,17 +3,44 @@
 #include "NodeManager.h"
 #include "api.h"
 #include "ros_lib/ros.h"
+#include "DataNodes/TankDriveNode.h"
+#include "DataNodes/ADIEncoderNode.h"
+#include "DataNodes/InertialSensorNode.h"
+#include "odometry/Odometry.h"
+#include "odometry/FollowerOdometry.h"
+#include "odometry/TankOdometry.h"
 
-class OdometryNode : public Node {
-private:    
-    std::string m_handle_name;
-
+class OdometryNode : public Node { 
 public:
-    OdometryNode(NodeManager* node_manager, std::string handle_name);
+    enum OdomConfig {
+        FOLLOWER, TANK
+    };
+
+    OdometryNode(NodeManager* node_manager, std::string handle_name, TankDriveNode* chassis, 
+        ADIEncoderNode* odom_encoder_1, ADIEncoderNode* odom_encoder_2, InertialSensorNode* inertial_sensor,
+        OdomConfig odom_config);
 
     void initialize();
 
-    void periodic();
+    Pose getCurrentPose();
+
+    void teleopPeriodic();
+
+    void autonPeriodic();
 
     ~OdometryNode();
+
+private:    
+    std::string m_handle_name;
+
+    OdomConfig m_odom_config;
+
+    TankDriveNode* m_chassis;
+    ADIEncoderNode* m_odom_encoder_1;
+    ADIEncoderNode* m_odom_encoder_2;
+    InertialSensorNode* m_inertial_sensor;
+
+    Odometry* m_odom;
+
+    Odometry* m_getOdomClass(OdomConfig odom_config);
 };
