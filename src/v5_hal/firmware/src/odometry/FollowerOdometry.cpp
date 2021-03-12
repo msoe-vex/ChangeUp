b5e35c79ff08fc8caf374475b39f6e3a1f6a6c37
Dylan Powers <dylanpowers125@gmail.com>
2021-03-11 22:08:58 -0600

    fixed an error in FollowerOdometry

--- src/v5_hal/firmware/src/odometry/FollowerOdometry.cpp
+++ src/v5_hal/firmware/src/odometry/FollowerOdometry.cpp
@@ -36,7 +36,7 @@ void FollowerOdometry::Update(double x_encoder_raw_ticks, double y_encoder_raw_t
     Vector2d robot_translation(x_delta, y_delta);
 
     // Update the current angle of the robot position
-    Odometry::m_robot_pose.angle = gyro_angle * Odometry::m_gyro_initial_angle.inverse() * GYRO_OFFSET;
+    Odometry::m_robot_pose.angle = gyro_angle * Odometry::m_gyro_initial_angle.inverse() * Eigen::Rotation2D(GYRO_OFFSET);
 
     // Rotate the translation vector by the current angle rotation matrix
     robot_translation = Odometry::m_robot_pose.angle * robot_translation;
