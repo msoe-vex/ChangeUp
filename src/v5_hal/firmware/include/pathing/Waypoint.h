0f2f93b6b3691337d01d566c0b6263f93102946a
Joseph Weller <wellerj@msoe.edu>
2021-03-19 22:30:59 -0500

    Updated waypoint class

--- src/v5_hal/firmware/include/pathing/Waypoint.h
+++ src/v5_hal/firmware/include/pathing/Waypoint.h
@@ -8,11 +8,14 @@ class Waypoint {
 private:
     Pose m_position;
     Vector2d m_velocity;
+    float m_time;
 
 public:
-    Waypoint(Pose position, Vector2d velocity);
+    Waypoint(Pose position, Vector2d velocity, float time);
 
     Pose getPosition();
 
     Vector2d getVelocity();
+
+    float getTime();
 };
--- src/v5_hal/firmware/src/pathing/Waypoint.cpp
+++ src/v5_hal/firmware/src/pathing/Waypoint.cpp
@@ -1,13 +1,19 @@
 #include "pathing/Waypoint.h"
 
-Waypoint::Waypoint(Pose position, Vector2d velocity) {
-
+Waypoint::Waypoint(Pose position, Vector2d velocity, float time) {
+    m_position = position;
+    m_velocity = velocity;
+    m_time = time;
 }
 
 Pose Waypoint::getPosition() {
-
+    return m_position;
 }
 
 Vector2d Waypoint::getVelocity() {
-    
+    return m_velocity;
+}
+
+float Waypoint::getTime() {
+    return m_time;
 }
