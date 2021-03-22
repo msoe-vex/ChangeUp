29eefeb85c08849173b843a40fce11ea489824ff
Andrew Kempen <akemperfi@gmail.com>
2021-03-21 22:47:59 -0500

    Created Path and PathPoint classes

--- src/v5_hal/firmware/include/auton/auton_actions/FollowPathAction.h
+++ src/v5_hal/firmware/include/auton/auton_actions/FollowPathAction.h
@@ -1,24 +1,23 @@
-#pragma once
+// #pragma once
 
-#include "auton/Auton.h"
-#include "nodes/subsystems/drivetrain_nodes/TankDriveNode.h"
-#include "nodes/odometry_nodes/OdometryNode.h"
-#include "adaptive_pursuit_controller/AdaptivePursuit.h"
+// #include "auton/Auton.h"
+// #include "nodes/subsystems/drivetrain_nodes/TankDriveNode.h"
+// #include "nodes/odometry_nodes/OdometryNode.h"
 
-class FollowPathAction : public AutonAction {
-private:
-    TankDriveNode* m_tank_drive;
-    OdometryNode* m_odom_node;
+// class FollowPathAction : public AutonAction {
+// private:
+//     TankDriveNode* m_tank_drive;
+//     OdometryNode* m_odom_node;
 
-    AdaptivePursuit m_controller;
+//     AdaptivePursuit m_controller;
 
-    string m_printString;
+//     string m_printString;
 
-public:
-    FollowPathAction(TankDriveNode* tank_drive, OdometryNode* odom_node, Path path, bool reversed = false, double wheelDiameter = 4.0625, 
-        double fixedLookahead = 10, double pathCompletionTolerance = 0.25, bool gradualStop = true);
+// public:
+//     FollowPathAction(TankDriveNode* tank_drive, OdometryNode* odom_node, Path path, bool reversed = false, double wheelDiameter = 4.0625, 
+//         double fixedLookahead = 10, double pathCompletionTolerance = 0.25, bool gradualStop = true);
 
-    void ActionInit();
-    actionStatus Action();
-    void ActionEnd();
-};
+//     void ActionInit();
+//     actionStatus Action();
+//     void ActionEnd();
+// };
---
+++ src/v5_hal/firmware/include/pathing/PathPoint.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "eigen/Eigen/Dense"
+#include "math/Pose.h"
+
+class PathPoint {
+public:
+    PathPoint(float time, Pose pose, Vector2d linear_velocity, float rotational_velocity);
+
+    float getTime();
+
+    Pose getPose();
+
+    Vector2d getLinearVelocity();
+
+    float getRotationalVelocity();
+
+    PathPoint interpolateTo(PathPoint other, float time);
+private:
+    Pose m_pose;
+    float m_time;
+    Vector2d m_linear_velocity;
+    float m_rotational_velocity;
+};
