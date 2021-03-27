a621bd53568f802b602aa0826234c174954aeaa0
NathanDuPont <nathan.dupont01@gmail.com>
2021-03-26 23:06:08 -0500

    Removed odom logging

--- src/v5_hal/firmware/src/nodes/odometry_nodes/OdometryNode.cpp
+++ src/v5_hal/firmware/src/nodes/odometry_nodes/OdometryNode.cpp
@@ -49,7 +49,7 @@ void OdometryNode::teleopPeriodic() {
     m_odom->Update(m_odom_encoder_1->getValue(), m_odom_encoder_2->getValue(), current_angle);
     //m_odom->Update(m_motor_1->getPosition(), m_motor_2->getPosition(), current_angle);
 
-    Logger::logInfo("Robot position: " + std::to_string(m_odom->GetPose().position.x()) + " " + std::to_string(m_odom->GetPose().position.y()));
+    //Logger::logInfo("Robot position: " + std::to_string(m_odom->GetPose().position.x()) + " " + std::to_string(m_odom->GetPose().position.y()));
 }
 
 void OdometryNode::autonPeriodic() {
