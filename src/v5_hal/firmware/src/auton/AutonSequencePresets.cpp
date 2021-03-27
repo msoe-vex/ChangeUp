40d34eccde7ef81a7d7aaf99a75d22b8000002fc
Joseph Weller <wellerj@msoe.edu>
2021-03-27 07:17:58 -0500

    Initial Autonomous

--- src/v5_hal/firmware/src/auton/AutonSequencePresets.cpp
+++ src/v5_hal/firmware/src/auton/AutonSequencePresets.cpp
@@ -1,9 +1,10 @@
 #include "auton/AutonSequencePresets.h"
 
-AutonSequence::AutonSequenceList getGoalScoringSequence(AutonNode* initial_node, IntakeNode* intake_node, ConveyorNode* conveyor_node) {
+AutonNode* getGoalScoringSequence(AutonNode* initial_node, IntakeNode* intake_node, ConveyorNode* conveyor_node) {
     AutonSequence* goal_scoring_sequence = new AutonSequence(initial_node);
 
     goal_scoring_sequence->AddAction(new OpenIntakesAction(intake_node));
+    goal_scoring_sequence->AddAction(new IntakeAction(intake_node, 0));
 
     goal_scoring_sequence->AddNext(new AutonNode(3., new WaitAction(3.)));
     goal_scoring_sequence->AddAction(new UpdateConveyorStateAction(conveyor_node, ConveyorNode::SCORING));
@@ -14,5 +15,5 @@ AutonSequence::AutonSequenceList getGoalScoringSequence(AutonNode* initial_node,
 
     goal_scoring_sequence->AddNext(new AutonNode(0.5, new OpenIntakesAction(intake_node)));
 
-    return goal_scoring_sequence->GetSequence();
+    return goal_scoring_sequence->GetSequence().tailNode;
 }
