66f358979176ce08606cc822d28bfe3007fa5047
msoevex <msoevex@gmail.com>
2021-06-10 04:20:13 +0000

    Updated compute script

--- scripts/compute-startup.sh
+++ scripts/compute-startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# Run 'crontab -e' and add this script with a @reboot tag to run on startup
-source /opt/ros/noetic/setup.sh
+# Run 'crontab -e' and add this script with a @reboot tag to run on startup. Make sure the
+# crontab file ends with an empty line
+sleep 30
+source /opt/ros/noetic/setup.bash
 source /home/ubuntu/ChangeUp/devel/setup.bash
 roslaunch /home/ubuntu/ChangeUp/src/change_up_driver_control/launch/15_inch_robot.launch
+
