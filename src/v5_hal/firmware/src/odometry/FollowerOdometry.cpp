111303353d205bb583ea9e54cb905a678395d18d
NathanDuPont <nathan.dupont01@gmail.com>
2021-06-13 18:20:09 -0500

    Updated main with new motor locations and print logic in FollowerOdometry

--- src/v5_hal/firmware/src/odometry/FollowerOdometry.cpp
+++ src/v5_hal/firmware/src/odometry/FollowerOdometry.cpp
@@ -37,26 +37,39 @@ void FollowerOdometry::Update(double x_encoder_raw_ticks, double y_encoder_raw_t
     Rotation2Dd angle_delta = gyro_angle * m_robot_pose.angle.inverse(); // Find change in angle
 
     double x_arc_length = x_encoder_location.norm() * angle_delta.angle();
-    double y_arc_length = -1 * y_encoder_location.norm() * angle_delta.angle();
+    double y_arc_length = y_encoder_location.norm() * angle_delta.angle();
 
-    double x_position_coef = fabs(sin(atan(x_encoder_location.y() / x_encoder_location.x())));
-    double y_position_coef = fabs(cos(atan(y_encoder_location.y() / y_encoder_location.x())));
+    // Need to create a dist to ticks
+    // double x_arc_length_ticks = x_arc_length
 
+    double x_position_coef = fabs(sin(atan(x_encoder_location.y() / x_encoder_location.x()))); // 48.14252608888209 for 90 deg
+    double y_position_coef = fabs(cos(atan(y_encoder_location.y() / y_encoder_location.x()))); // 28.99733373232959 for 90 deg
+
+    // -6.8475
+    // -1.42578274 rad
+    // -0.98950394259
+
+    // 2.2422857
+    // 1.15129588 rad
+    // 0.40730426569
+
+    // Translate to encoder value
     double x_encoder_turning_component = x_arc_length * x_position_coef;
     double y_encoder_turning_component = y_arc_length * y_position_coef;
 
     double x_delta = x_encoder_delta - x_encoder_turning_component;
     double y_delta = y_encoder_delta - y_encoder_turning_component;
 
-    double calculated_x_coef = x_encoder_dist / (x_encoder_location.norm() * gyro_angle.angle());
-    double calculated_y_coef = y_encoder_dist / (y_encoder_location.norm() * gyro_angle.angle());
+    double calculated_x_coef = x_encoder_dist / (x_encoder_location.norm() * gyro_angle.angle()); 
+    double calculated_y_coef = y_encoder_dist / (y_encoder_location.norm() * gyro_angle.angle()); 
 
-    Logger::logInfo("x_encoder_delta: " + std::to_string(total_x) + 
-                    " | x_encoder_turning_component: " + std::to_string(total_y) +
+    Logger::logInfo("x_encoder_dist: " + std::to_string(x_encoder_dist) + 
+                    " | x_encoder_turning_component: " + std::to_string(x_encoder_turning_component) +
                     " | x_coefficient: " + std::to_string(calculated_x_coef) +
-                    " | y_encoder_delta: " + std::to_string(turn_x) + 
-                    " | y_coefficient: " + std::to_string(turn_y) +
-                    " | y_coefficient: " + std::to_string(calculated_y_coef));
+                    " | y_encoder_dist: " + std::to_string(y_encoder_dist) + 
+                    " | y_encoder_turning_component: " + std::to_string(y_encoder_turning_component) +
+                    " | y_coefficient: " + std::to_string(calculated_y_coef) + 
+                    " | angle:" + std::to_string(gyro_angle.angle()));
 
     // total_x = total_x + x_encoder_delta;
     // total_y = total_y + y_encoder_delta;
