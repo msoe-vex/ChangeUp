9ceae009570f380d1692d6e7eed783e594fa9373
Jonathan Phung <phungj@msoe.edu>
2021-06-18 21:03:43 -0500

    Explicitly extracted out components of vector2d objects for testing

--- src/v5_hal/firmware/src/pathing/PathPoint.cpp
+++ src/v5_hal/firmware/src/pathing/PathPoint.cpp
@@ -27,15 +27,15 @@ PathPoint PathPoint::interpolateTo(PathPoint other, float time) {
     float t = time - getTime();
     float deltaTime = other.getTime() - getTime();
 
-    Vector2d acceleration = deltaTime == 0. ? Vector2d(0., 0.) : ((other.getLinearVelocity() + getLinearVelocity()) / deltaTime);
-    Vector2d position = 0.5 * acceleration * (t * t) + getLinearVelocity() * t + getPose().position;
-    Vector2d velocity = acceleration * t + getLinearVelocity();
+    Eigen::Vector2d acceleration = deltaTime == 0. ? Eigen::Vector2d(0., 0.) : Eigen::Vector2d((other.getLinearVelocity().x() + getLinearVelocity().x()) / deltaTime, (other.getLinearVelocity().y(), getLinearVelocity().y()) / deltaTime);
+    Eigen::Vector2d position = 0.5 * acceleration * (t * t) + getLinearVelocity() * t + getPose().position;
+    Eigen::Vector2d velocity = acceleration * t + getLinearVelocity();
 
     // Logger::logInfo("Time: " + std::to_string(time) + " | position: " + std::to_string(position.x()) + " " + std::to_string(position.y()) + 
     //     " | velocity: " + std::to_string(velocity.x()) + " " + std::to_string(velocity.y()));
 
     float alpha = deltaTime == 0. ? 0. : (other.getRotationalVelocity() - getRotationalVelocity()) / deltaTime;
-    Rotation2Dd theta = Rotation2Dd(0.5 * alpha * (t * t) + getRotationalVelocity() * t) * getPose().angle;
+    Eigen::Rotation2Dd theta = Eigen::Rotation2Dd(0.5 * alpha * (t * t) + getRotationalVelocity() * t) * getPose().angle;
     float omega = alpha * t + getRotationalVelocity();
 
     return PathPoint(time, Pose(position, theta), velocity, omega);
