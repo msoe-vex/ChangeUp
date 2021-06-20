bf216f48c2be8f4b172d0dffae10d3e494331b83
Jonathan Phung <phungj@msoe.edu>
2021-06-19 19:09:44 -0500

    Refactored tests according to MR review, migrated fixtures into tests themselves, added generic fixture, added documentation

--- src/v5_hal/test/pathing/test_PathPoint.cpp
+++ src/v5_hal/test/pathing/test_PathPoint.cpp
@@ -1,75 +1,27 @@
 #include <gtest/gtest.h>
 #include "pathing/PathPoint.h"
 
-class xChangeConstantVelModule : public ::testing::Test {
-    protected:
-        PathPoint* firstPoint;
-        PathPoint* secondPoint;
+// This is the default fixture for the PathPointTest, intentionally left empty for now
+class PathPointTest : public ::testing::Test {
+    virtual void SetUp() {
 
-        virtual void SetUp() {
-            // Creates two points that only have a positive change in x with constant velocity
-            firstPoint = new PathPoint(0, Pose(Eigen::Vector2d(0, 0), Eigen::Rotation2Dd(0)), Eigen::Vector2d(1, 0), 0);
-            secondPoint = new PathPoint(1, Pose(Eigen::Vector2d(1, 0), Eigen::Rotation2Dd(0)), Eigen::Vector2d(1, 0), 0);
-        }
+    }
 
-        virtual void TearDown() {
-            delete firstPoint;
-            delete secondPoint;
-        }
+    virtual void TearDown() {
+
+    }
 };
 
-class yChangeConstantVelModule : public ::testing::Test {
-    protected:
-        PathPoint* firstPoint;
-        PathPoint* secondPoint;
+/**
+ * @brief Given two points with only constant x velocities, when the first PathPoint is interpolated to the second PathPoint at the second's time, then the resulting PathPoint should be equivalent to the second point.
+ * @param PathPointTest The fixture used for testing PathPoints.
+ * @param xChangeConstantVel The name of this test.
+ * @author Jonathan Phung
+ */
 
-        virtual void SetUp() {
-            // Creates two points that only have a positive change in y with constant velocity
-            firstPoint = new PathPoint(0, Pose(Eigen::Vector2d(0, 0), Eigen::Rotation2Dd(0)), Eigen::Vector2d(0, 1), 0);
-            secondPoint = new PathPoint(1, Pose(Eigen::Vector2d(0, 1), Eigen::Rotation2Dd(0)), Eigen::Vector2d(0, 1), 0);
-        }
-
-        virtual void TearDown() {
-            delete firstPoint;
-            delete secondPoint;
-        }
-};
-
-class xyChangeConstantVelModule : public ::testing::Test {
-    protected:
-        PathPoint* firstPoint;
-        PathPoint* secondPoint;
-
-        virtual void SetUp() {
-            // Creates two points that have a positive change in x and y with constant velocity
-            firstPoint = new PathPoint(0, Pose(Eigen::Vector2d(0, 0), Eigen::Rotation2Dd(0)), Eigen::Vector2d(1, 1), 0);
-            secondPoint = new PathPoint(1, Pose(Eigen::Vector2d(1, 1), Eigen::Rotation2Dd(0)), Eigen::Vector2d(1, 1), 0);
-        }
-
-        virtual void TearDown() {
-            delete firstPoint;
-            delete secondPoint;
-        }
-};
-
-class noChangeModule : public ::testing::Test {
-    protected:
-        PathPoint* firstPoint;
-        PathPoint* secondPoint;
-
-        virtual void SetUp() {
-            // Creates two points that do not change
-            firstPoint = new PathPoint(0, Pose(Eigen::Vector2d(0, 0), Eigen::Rotation2Dd(0)), Eigen::Vector2d(0, 0), 0);
-            secondPoint = new PathPoint(1, Pose(Eigen::Vector2d(0, 0), Eigen::Rotation2Dd(0)), Eigen::Vector2d(0, 0), 0);
-        }
-
-        virtual void TearDown() {
-            delete firstPoint;
-            delete secondPoint;
-        }
-};
-
-TEST_F(xChangeConstantVelModule, xChangeConstantVel) {
+TEST(PathPointTest, xChangeConstantVel) {
+    PathPoint* firstPoint = new PathPoint(0, Pose(Eigen::Vector2d(0, 0), Eigen::Rotation2Dd(0)), Eigen::Vector2d(1, 0), 0);
+    PathPoint* secondPoint = new PathPoint(1, Pose(Eigen::Vector2d(1, 0), Eigen::Rotation2Dd(0)), Eigen::Vector2d(1, 0), 0);
     PathPoint result = firstPoint->interpolateTo(*secondPoint, 1);
 
     EXPECT_EQ(secondPoint->getLinearVelocity().x(), result.getLinearVelocity().x());
@@ -79,9 +31,22 @@ TEST_F(xChangeConstantVelModule, xChangeConstantVel) {
     EXPECT_EQ(secondPoint->getPose().angle.angle(), result.getPose().angle.angle());
     EXPECT_EQ(secondPoint->getRotationalVelocity(), result.getRotationalVelocity());
     EXPECT_EQ(secondPoint->getTime(), result.getTime());
+
+    delete firstPoint;
+    delete secondPoint;
 }
 
-TEST_F(yChangeConstantVelModule, yChangeConstantVel) {
+
+/**
+ * @brief Given two points with only constant y velocities, when the first PathPoint is interpolated to the second PathPoint at the second's time, then the resulting PathPoint should be equivalent to the second point.
+ * @param PathPointTest The fixture used for testing PathPoints.
+ * @param yChangeConstantVel The name of this test.
+ * @author Jonathan Phung
+ */
+
+TEST(PathPointTest, yChangeConstantVel) {
+    PathPoint* firstPoint = new PathPoint(0, Pose(Eigen::Vector2d(0, 0), Eigen::Rotation2Dd(0)), Eigen::Vector2d(0, 1), 0);
+    PathPoint* secondPoint = new PathPoint(1, Pose(Eigen::Vector2d(0, 1), Eigen::Rotation2Dd(0)), Eigen::Vector2d(0, 1), 0);
     PathPoint result = firstPoint->interpolateTo(*secondPoint, 1);
 
     EXPECT_EQ(secondPoint->getLinearVelocity().x(), result.getLinearVelocity().x());
@@ -91,10 +56,23 @@ TEST_F(yChangeConstantVelModule, yChangeConstantVel) {
     EXPECT_EQ(secondPoint->getPose().angle.angle(), result.getPose().angle.angle());
     EXPECT_EQ(secondPoint->getRotationalVelocity(), result.getRotationalVelocity());
     EXPECT_EQ(secondPoint->getTime(), result.getTime());
+
+    delete firstPoint;
+    delete secondPoint;
 }
 
-TEST_F(xyChangeConstantVelModule, xyChangeConstantVel) {
-    PathPoint result = firstPoint -> interpolateTo(*secondPoint, 1);
+
+/**
+ * @brief Given two points with only constant x and y velocities, when the first PathPoint is interpolated to the second PathPoint at the second's time, then the resulting PathPoint should be equivalent to the second point.
+ * @param PathPointTest The fixture used for testing PathPoints.
+ * @param xyChangeConstantVel The name of this test.
+ * @author Jonathan Phung
+ */
+
+TEST(PathPointTest, xyChangeConstantVel) {
+    PathPoint* firstPoint = new PathPoint(0, Pose(Eigen::Vector2d(0, 0), Eigen::Rotation2Dd(0)), Eigen::Vector2d(1, 1), 0);
+    PathPoint* secondPoint = new PathPoint(1, Pose(Eigen::Vector2d(1, 1), Eigen::Rotation2Dd(0)), Eigen::Vector2d(1, 1), 0);
+    PathPoint result = firstPoint->interpolateTo(*secondPoint, 1);
 
     EXPECT_EQ(secondPoint->getLinearVelocity().x(), result.getLinearVelocity().x());
     EXPECT_EQ(secondPoint->getLinearVelocity().y(), result.getLinearVelocity().y());
@@ -103,10 +81,22 @@ TEST_F(xyChangeConstantVelModule, xyChangeConstantVel) {
     EXPECT_EQ(secondPoint->getPose().angle.angle(), result.getPose().angle.angle());
     EXPECT_EQ(secondPoint->getRotationalVelocity(), result.getRotationalVelocity());
     EXPECT_EQ(secondPoint->getTime(), result.getTime());
+
+    delete firstPoint;
+    delete secondPoint;
 }
 
-TEST_F(noChangeModule, noChange) {
-    PathPoint result = firstPoint -> interpolateTo(*secondPoint, 1);
+/**
+ * @brief Given two points with no velocity or acceleration, when the first PathPoint is interpolated to the second PathPoint at the second's time, then the resulting PathPoint should be equivalent to the second point.
+ * @param PathPointTest The fixture used for testing PathPoints.
+ * @param noChange The name of this test.
+ * @author Jonathan Phung
+ */
+
+TEST(PathPointTest, noChange) {
+    PathPoint* firstPoint = new PathPoint(0, Pose(Eigen::Vector2d(0, 0), Eigen::Rotation2Dd(0)), Eigen::Vector2d(0, 0), 0);
+    PathPoint* secondPoint = new PathPoint(1, Pose(Eigen::Vector2d(0, 0), Eigen::Rotation2Dd(0)), Eigen::Vector2d(0, 0), 0);
+    PathPoint result = firstPoint->interpolateTo(*secondPoint, 1);
 
     EXPECT_EQ(secondPoint->getLinearVelocity().x(), result.getLinearVelocity().x());
     EXPECT_EQ(secondPoint->getLinearVelocity().y(), result.getLinearVelocity().y());
@@ -115,4 +105,7 @@ TEST_F(noChangeModule, noChange) {
     EXPECT_EQ(secondPoint->getPose().angle.angle(), result.getPose().angle.angle());
     EXPECT_EQ(secondPoint->getRotationalVelocity(), result.getRotationalVelocity());
     EXPECT_EQ(secondPoint->getTime(), result.getTime());
+
+    delete firstPoint;
+    delete secondPoint;
 }
