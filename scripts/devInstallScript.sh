de2938aede4c6674a0c0857b79366505f85d210e
Nathan <Nathan.DuPont01@gmail.com>
2021-09-02 08:24:28 -0500

    Adding curl install to script

--- scripts/devInstallScript.sh
+++ scripts/devInstallScript.sh
@@ -3,6 +3,9 @@
 # Installs ROS, PROS and gcc-arm compiler for V5 builds
 
 # Install ROS
+sudo apt update
+sudo apt upgrade -y
+sudo apt install curl
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
 sudo apt update
