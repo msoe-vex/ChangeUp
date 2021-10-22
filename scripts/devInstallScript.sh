df851b1bac4d279147b615aed6980dc258ab4c40
NathanDuPont <nathan.dupont01@gmail.com>
2021-10-22 00:44:13 -0500

    Adding update to install wget in the script

--- scripts/devInstallScript.sh
+++ scripts/devInstallScript.sh
@@ -5,7 +5,7 @@
 # Install ROS
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install curl -y
+sudo apt-get install curl wget -y
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
 sudo apt-get update
