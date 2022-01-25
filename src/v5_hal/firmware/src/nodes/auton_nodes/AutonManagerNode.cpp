0540b932214aeb2a7be74a725f5e7aeacb197e1a
NathanDuPont <nathan.dupont01@gmail.com>
2022-01-25 15:27:23 -0600

    Removing extra pose reset code and changing default auton

--- src/v5_hal/firmware/src/nodes/auton_nodes/AutonManagerNode.cpp
+++ src/v5_hal/firmware/src/nodes/auton_nodes/AutonManagerNode.cpp
@@ -4,7 +4,7 @@ AutonManagerNode::AutonManagerNode(NodeManager* node_manager, IDriveNode* drive_
         IntakeNode* intake_node, OdometryNode* odometry_node, InertialSensorNode* inertial_sensor_node) : Node(node_manager, 50) {
     m_test_path_auton = new TestPathAuton(drive_node, odometry_node);
     m_prog_skills = new ProgrammingSkillsAuton(drive_node, odometry_node, conveyor_node, intake_node, inertial_sensor_node);
-    selected_auton = m_prog_skills;
+    selected_auton = m_test_path_auton;
 }
 
 void AutonManagerNode::initialize() {
